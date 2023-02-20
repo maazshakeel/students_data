921b26e4a38aa0aa08fa313e74ab9c750bc9faf9
Maaz <62890173+maazshakeel@users.noreply.github.com>
2023-02-20 13:05:28 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\NoteController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,24 +14,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', [HomeController::class, 'index']);
-
-Route::get('/notes', [NoteController::class, 'index']);
-
-Route::get('/notes/create', [NoteController::class, 'create']);
-
-Route::get('/notes/user-list', [NoteController::class, 'userList'])->middleware('is_admin');
-
-Route::get('/notes/{id}', [NoteController::class, 'show']);
-
-Route::post('/notes', [NoteController::class, 'store']);
-
-Route::get('/notes/{id}/edit', [NoteController::class, 'edit']);
-
-Route::patch('/notes/{id}', [NoteController::class, 'update']);
-
-Route::delete('/notes/{id}', [NoteController::class, 'delete']);
 
 // CrUD
 Route::resource('students', StudentController::class);
